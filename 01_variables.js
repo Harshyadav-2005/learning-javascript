fc4904e23102d6624b302038a2eae922e9715625
Harsh Yadav <harshyadav3025@gmail.com>
2024-09-26 00:49:21 +0530

    commit

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const AccountId = 324233
+let accountEmail = "siuuu@gmail.com"
+var accountPassword = "54353"
+accountCity = "srinagar"
+let accountState;
+
+// accountID = 2 // not allowed 
+
+accountEmail = "messi@gmail.com"
+accountPassword = "24455"
+accountCity = "udaipur"
+console.table([AccountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+prefer not to use var
+beacuse of issue in block scope and functional scope
+*/
