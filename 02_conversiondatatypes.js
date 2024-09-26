73925f28ac6d60b899aa64c426e418a0889d66f3
Harsh Yadav <harshyadav3025@gmail.com>
2024-09-26 22:28:49 +0530

    conversion of datatypes

---
+++ 02_conversiondatatypes.js
@@ -0,0 +1,22 @@
+let score ="69"
+console.log(typeof score)
+
+let value = Number(score)
+console.log(typeof value);
+console.log(value);
+
+// "69" --> 69
+// "harsh" --> NaN
+// true,false --> 1,0
+
+let bool = ""
+let isbool = Boolean(bool)
+console.log(isbool)
+
+// "" --> false
+// "harsh" --> true
+// 0,1 --> false,true
+
+let siuu = 3746
+let messi = String(siuu)
+console.log(typeof messi)
